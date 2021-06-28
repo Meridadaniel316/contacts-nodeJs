3563a9c290fdd8ebd552e5b4125eb97d62cae4dc
MeridaDaniel <Dacastamerd@gmail.com>
2021-06-28 16:12:02 -0500

    Se implemento el formulario para crear un contacto

--- src/routers/index.js
+++ src/routers/index.js
@@ -5,4 +5,9 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+router.post('/new-contact', (req, res) => {
+    console.log(req.body);
+    res.send('received');
+})
+
 module.exports = router;
