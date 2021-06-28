0f1b7f779f1aff74ff83baa8f530cac9757dca52
MeridaDaniel <Dacastamerd@gmail.com>
2021-06-28 18:15:48 -0500

    Se mejoro la interfaz

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 const router = Router();
 const admin = require('firebase-admin');
 
+let now = new Date();
+
 admin.initializeApp({
     credential: admin.credential.applicationDefault(),
     databaseURL: 'https://node-firebase-example-bab32-default-rtdb.firebaseio.com/'
@@ -14,16 +16,17 @@ router.get('/', (req, res) => {
         const data = snapshot.val();
         res.render('index', { contacts: data});
     });
-    
 });
 
 router.post('/new-contact', (req, res) => {
     console.log(req.body);
+    
     const newContact = {
         firstname: req.body.firstname,
         lastname: req.body.lastname,
         email: req.body.email,
-        phone: req.body.phone
+        phone: req.body.phone,
+        date: now.toUTCString()
     };
     db.ref('contacts').push(newContact);
     res.redirect('/');
