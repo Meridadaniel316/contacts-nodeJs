9ba60e1a2e743305d9064685ca56d5dc5568eebf
MeridaDaniel <Dacastamerd@gmail.com>
2021-06-28 16:35:37 -0500

    Se agrego la base de datos firebase

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,13 @@
 const { Router } = require('express');
 const router = Router();
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://node-firebase-example-bab32-default-rtdb.firebaseio.com/'
+});
+
+const db = admin.database();
 
 router.get('/', (req, res) => {
     res.render('index');
@@ -7,7 +15,15 @@ router.get('/', (req, res) => {
 
 router.post('/new-contact', (req, res) => {
     console.log(req.body);
+    const newContact = {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        phone: req.body.phone
+    };
+    db.ref('contacts').push(newContact);
     res.send('received');
 })
 
 module.exports = router;
+
